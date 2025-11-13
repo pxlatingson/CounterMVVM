95180313c2d6fca4d3d63b7ab10b03f5e1026224
Alfred Tingson <alfredtingson@pixelmine.org>
2025-11-13 14:25:42 +0800

    CounterApp composable, CounterViewModel parameter, bind increment/decrement to the buttons

--- app/src/main/java/com/example/countermvvm/MainActivity.kt
+++ app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.activity.viewModels
 import com.example.countermvvm.ui.theme.CounterMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +24,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CounterMVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val viewModel: CounterViewModel by viewModels()
+                CounterMVVMTheme {
+                    CounterApp(
+                        viewModel = viewModel,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CounterApp(viewModel: CounterViewModel, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = viewModel.count.value.toString(),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(
+            onClick = { viewModel.increment() },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Increment")
+        }
+        Button(
+            onClick = { viewModel.decrement() },
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text("Decrement")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CounterAppPreview() {
     CounterMVVMTheme {
-        Greeting("Android")
+        val viewModel = CounterViewModel()
+        CounterApp(viewModel = viewModel)
     }
 }
